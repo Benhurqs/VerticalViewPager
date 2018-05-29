b1cc97f1f95becf66998e0dd3230762077094140
Benhur Quintino de Souza <benhur.souza@localhost.localdomain>
2018-05-29 17:42:17 -0300

    Initial commit

---
+++ app/src/main/java/com/benhurqs/verticalviewpager/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.benhurqs.verticalviewpager
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //https://cdn-images-1.medium.com/max/800/1*hLJ9YuOPYlbcNGMdzUh7Jw.gif
+}
